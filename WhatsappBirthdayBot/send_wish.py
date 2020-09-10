c50c6f51cc641f5b263910396b1a437a7e4dbb8b
sooryaprakash31 <sooryaprakash.r31@gmail.com>
2020-09-10 23:56:08 +0530

    Fixed multi line message feature

--- WhatsappBirthdayBot/send_wish.py
+++ WhatsappBirthdayBot/send_wish.py
@@ -8,17 +8,18 @@ class BirthdayBot():
     def __init__(self):        
         self.username=input("Enter Contact Name: ")
         print("Enter Wish Text: ")
+        self.lines = []
+        while True:
+            line = input()
+            if line:
+                self.lines.append(line)
+            else:
+                break
         profile = webdriver.FirefoxProfile('/home/soorya/.mozilla/firefox/1gc0tunv.default')
         self.driver = webdriver.Firefox(profile)
         self.driver.get("https://web.whatsapp.com/")
         sleep(20)
         self.sendWish()
-
-    def getWishText(self):
-        wish = ''' 
-        Lorem ipsum
-        '''
-        return wish
 
     def sendWish(self):
         try:
@@ -27,9 +28,12 @@ class BirthdayBot():
             print(e)
         contact.click()
         sleep(1)
-        
-        self.driver.find_elements_by_xpath('/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div[2]/div/div[2]')[0].send_keys("Your are the winner!!!"+ Keys.SHIFT + Keys.ENTER +"Contact 12345 for assistance")
-        sleep(10)
-        self.driver.find_elements_by_xpath('/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div[3]/button')[0].click()
+        textBoxPath = '/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div[2]/div/div[2]'
+        for line in self.lines:
+            self.driver.find_elements_by_xpath(textBoxPath)[0].send_keys(line)
+            self.driver.find_elements_by_xpath(textBoxPath)[0].send_keys(Keys.SHIFT+Keys.ENTER)
+        sleep(5)
+        sendButtonPath='/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div[3]/button'
+        self.driver.find_elements_by_xpath(sendButtonPath)[0].click()
         
 BirthdayBot()
