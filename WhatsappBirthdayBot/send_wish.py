46410144b0ec4f23af3616e1b54f2e52ffc8d490
sooryaprakash31 <sooryaprakash.r31@gmail.com>
2020-09-11 11:45:11 +0530

    Scheduling the message

--- WhatsappBirthdayBot/send_wish.py
+++ WhatsappBirthdayBot/send_wish.py
@@ -15,6 +15,7 @@ class BirthdayBot():
                 self.lines.append(line)
             else:
                 break
+        self.date=input("Sending Date (yyyy-mm-dd): ")
         profile = webdriver.FirefoxProfile('/home/soorya/.mozilla/firefox/1gc0tunv.default')
         self.driver = webdriver.Firefox(profile)
         self.driver.get("https://web.whatsapp.com/")
@@ -27,12 +28,18 @@ class BirthdayBot():
         except Exception as e:
             print(e)
         contact.click()
-        sleep(1)
+        sleep(2)
         textBoxPath = '/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div[2]/div/div[2]'
         for line in self.lines:
             self.driver.find_elements_by_xpath(textBoxPath)[0].send_keys(line)
             self.driver.find_elements_by_xpath(textBoxPath)[0].send_keys(Keys.SHIFT+Keys.ENTER)
         sleep(5)
+        while True:
+            currentTime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            self.time = self.date + " 00:00:00"
+            if currentTime==self.time:
+                break
+            sleep(1)
         sendButtonPath='/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div[3]/button'
         self.driver.find_elements_by_xpath(sendButtonPath)[0].click()
         
