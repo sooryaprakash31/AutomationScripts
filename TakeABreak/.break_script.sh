8b29a45b958a11b9fb12fc0d1c0e7a5c109c2d39
sooryaprakash31 <sooryaprakash.r31@gmail.com>
2020-09-13 22:22:22 +0530

    Added validation in break script

--- TakeABreak/.break_script.sh
+++ TakeABreak/.break_script.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
 function breakmode(){
+    zero=0
     read -p "Work time: " worktime
-    read -p "Break time: " breaktime
     work=`python3 converter.py $worktime`
+
+    if [ $work == $zero ] 
+    then
+        echo "Invalid work time"
+        return
+    fi
+    read -p "Break time: " breaktime  
     break=`python3 converter.py $breaktime`
+    if [ $break == $zero ] 
+    then
+        echo "Invalid break time"
+        return
+    fi
     echo "Carry on. I will notify you when it's break time"
+    while true;do
     sleep $(($work-10))
     python3 desktop_notifier.py
-    sleep 10s 
+    sleep 10s
     sudo rtcwake -u -s $break -m mem
+    done
 }
